a2fafa283902b4f9f27ba0fbe5d005cec888a7aa
Brandon Haynes <bhaynes@cs.washington.edu>
2015-02-10 14:55:00 -0800

    Moved type map to base schema

--- transformer/schema.py
+++ transformer/schema.py
@@ -10,3 +10,19 @@ class Schema(object):
     self.nullable = nullable
     self.attributes = kwargs
     self.attributes['args'] = args
+
+  @property
+  def type_map(self):
+      return {'int64':  'LONG_TYPE', 
+              'int32':  'INT_TYPE', 
+              'uint64': 'LONG_TYPE', 
+              'uint32': 'INT_TYPE', 
+              'float':  'FLOAT_TYPE',
+              'double': 'DOUBLE_TYPE'} #TODO: What is Myria's story for nans?
+
+  @property
+  def _reverse_type_map(self):
+      return {'LONG_TYPE': 'int64', 
+              'INT_TYPE': 'int32', 
+              'FLOAT_TYPE': 'float',
+              'DOUBLE_TYPE': 'double'}
